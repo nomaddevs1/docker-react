e99227f09fc22a39a1373f06db590879f5552413
Farouq Siwoku <farouqsiwoku@gmail.com>
2021-06-11 16:42:42 -0500

    Docker project

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/How lovely. The game da/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/How lovely. The game da/i);
+  expect(linkElement).toBeInTheDocument();
+});
